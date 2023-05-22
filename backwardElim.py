37d30ba8c5b8ad7f8c8d390d10413643e8682b1a
CRIS66666 <122409604+CRIS66666@users.noreply.github.com>
2023-05-21 21:42:39 -0700

    Update backwardElim.py

--- backwardElim.py
+++ backwardElim.py
@@ -1,5 +1,34 @@
+from itertools import combinations
 from randAccuracy import accuracy
 
-def BE():
-	maxAcc = accuracy()
-	print(f"Using no features and “random” evaluation, I get an accuracy of: {maxAcc} ")
+def BE(featureNum):
+    featureSet = set(range(1, featureNum + 1))
+    bestSet = featureSet.copy()
+    bestAcc = accuracy()
+
+    print(f"Using all features and 'random' evaluation, I get an accuracy of: {bestAcc}%")
+    print("Beginning Search")
+
+    for i in range(featureNum, 0, -1):
+        maxAcc = -float('inf')
+        bestFeature = None
+
+        for currentSet in combinations(featureSet, i - 1):
+            temp = accuracy()
+            print(f"Using feature(s) {currentSet}, accuracy is {temp}%")
+
+            if temp > maxAcc:
+                maxAcc = temp
+                bestFeature = currentSet
+                print(f"Feature(s) {currentSet} achieved the best accuracy: {maxAcc}%")
+            else:
+                print("Warning, accuracy decreased!")
+
+        if maxAcc > bestAcc:
+            bestAcc = maxAcc
+            bestSet = set(bestFeature)
+        else:
+            break
+
+    print(f"\nBest feature subset: {bestSet}")
+    print(f"Best accuracy: {bestAcc}%")
