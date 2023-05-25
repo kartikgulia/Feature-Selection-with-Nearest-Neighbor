b78dcda0a8688d0822edc1d62fe2136982ba0a43
kartikgulia <kartik.gulia03@gmail.com>
2023-05-25 11:52:14 -0700

    fixed forwardSelection

--- forwardSelection.py
+++ forwardSelection.py
@@ -1,5 +1,6 @@
 from randAccuracy import accuracy
 from itertools import combinations
+import copy
 # def FS(featureNum):
 # 	maxAcc = accuracy()
 # 	maxFeature = 0
@@ -59,39 +60,74 @@ from itertools import combinations
 # 	print(f"Finished Search. Best Feature subset is {bestSet}, which has an accurary of {bestAcc}%")
 
 
-def FS(featureNum):
-	bestSet = set()
-	#bestAcc = 0.0
+# def FS(featureNum):
+# 	bestSet = set()
+# 	#bestAcc = 0.0
+# 	cycleSet = set()
 	
-	bestAcc = accuracy()
+# 	bestAcc = accuracy()
 	
-	print(f"Using no features and “random” evaluation, I get an accuracy of: {bestAcc}%")
-	print("Beginning Search")
+# 	print(f"Using no features and “random” evaluation, I get an accuracy of: {bestAcc}%")
+# 	print("Beginning Search")
 
-	for i in range(1, featureNum+1):
-		totalcombinations = combinations(range(1, featureNum+1), i)
+# 	for i in range(1, featureNum+1):
+# 		totalcombinations = combinations(range(1, featureNum+1), i)
 
-		for j in totalcombinations:
-			#print(f"\nj:{j}, bestSet: {bestSet}\n")
-			if (len(j) > 1 and bestSet.issubset(set(j))):
+# 		for j in totalcombinations:
+# 			#print(f"\nj:{j}, bestSet: {bestSet}\n")
+# 			if (len(j) > 1 and bestSet.issubset(set(j))):
 				
+# 				temp = accuracy()
+# 				print(f"Using feature(s) {j} accuracy is {temp}%")
+				
+
+# 				if (temp > bestAcc):
+# 						bestAcc = temp
+# 						bestSet = set(j)
+# 						print(f"Feature set {bestSet} was best, accuracy is {bestAcc}%")
+# 				else:
+# 					print(f"Warning, accuracy decreased!")
+# 			elif (len(j) == 1):
+# 				temp = accuracy()
+# 				print(f"Using feature(s) {j} accuracy is {temp}%")
+# 				if (temp > bestAcc):
+# 						bestAcc = temp
+# 						bestSet = set(j)
+# 						print(f"Feature set {bestSet} was best, accuracy is {bestAcc}%")
+# 				else:
+# 					print(f"Warning, accuracy decreased!")
+# 		# if (bestSet.issubset(set(j))):
+# 		# 	break
+# 	print(f"Finished Search. Best Feature subset is {bestSet}, which has an accurary of {bestAcc}%")
+
+def FS(featureNum):
+	bestSet = []
+	bestAcc = 0.0
+	currSet = []
+	print("Beginning Search.")
+	for i in range(featureNum):
+		currAcc = 0.0
+		bestChecker = -1
+		worseChecker = -1
+		for j in range(1, featureNum + 1):
+			if j not in currSet:
+				node = copy.deepcopy(currSet)
+				node.append(j)
 				temp = accuracy()
-				print(f"Using feature(s) {j} accuracy is {temp}%")
-				if (temp > bestAcc):
-						bestAcc = temp
-						bestSet = set(j)
-						print(f"Feature set {bestSet} was best, accuracy is {bestAcc}%")
-				else:
-					print(f"Warning, accuracy decreased!")
-			elif (len(j) == 1):
-				temp = accuracy()
-				print(f"Using feature(s) {j} accuracy is {temp}%")
-				if (temp > bestAcc):
-						bestAcc = temp
-						bestSet = set(j)
-						print(f"Feature set {bestSet} was best, accuracy is {bestAcc}%")
-				else:
-					print(f"Warning, accuracy decreased!")
-		# if (bestSet.issubset(set(j))):
-		# 	break
-	print(f"Finished Search. Best Feature subset is {bestSet}, which has an accurary of {bestAcc}%")
+				print(f"Using feature(s) {sorted(node)} accuracy is {temp}%")
+				if temp > currAcc:
+					currAcc = temp
+					worseChecker = j
+				if temp > bestAcc:
+					bestAcc = temp
+					bestChecker = j
+		if bestChecker >= 0:
+			currSet.append(bestChecker)
+			bestSet.append(bestChecker)
+			print(f"Feature set {sorted(currSet)} was best, accuracy is {bestAcc}%")
+		else:
+			currSet.append(worseChecker)
+			print(f"(Warning, Accuracy has decreased!!)")
+			print(f"Feature set {sorted(currSet)} was best, accuracy is {currAcc}%")
+
+	print(f"Finished Search! The best feature subset is {bestSet} which has an accuracy of {bestAcc}%")
