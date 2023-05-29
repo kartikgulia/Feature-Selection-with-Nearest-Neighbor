bc63ccbe40bf530d4a5428c161500e9ff10aa7dc
CRIS66666 <122409604+CRIS66666@users.noreply.github.com>
2023-05-28 22:05:30 -0700

    Fixed the bug

--- backwardElim.py
+++ backwardElim.py
@@ -2,33 +2,41 @@ from itertools import combinations
 from randAccuracy import accuracy
 
 def BE(featureNum):
-    featureSet = set(range(1, featureNum + 1))
-    bestSet = featureSet.copy()
+    bestSet = list(range(1, featureNum + 1))
     bestAcc = accuracy()
+    currSet = bestSet.copy()
+    print("Beginning Search.")
 
-    print(f"Using all features and 'random' evaluation, I get an accuracy of: {bestAcc}%")
-    print("Beginning Search")
+    if featureNum == 1:
+        print("Using feature(s) [1] accuracy is", bestAcc, "%")
+        print("Warning: Accuracy cannot be improved further by removing any feature!")
+        print("Finished Search! The best feature subset is [1] which has an accuracy of", bestAcc, "%")
+        return
 
-    for i in range(featureNum, 0, -1):
-        maxAcc = -float('inf')
-        bestFeature = None
-
-        for currentSet in combinations(featureSet, i - 1):
-            temp = accuracy()
-            print(f"Using feature(s) {currentSet}, accuracy is {temp}%")
-
-            if temp > maxAcc:
-                maxAcc = temp
-                bestFeature = currentSet
-                print(f"Feature(s) {currentSet} achieved the best accuracy: {maxAcc}%")
-            else:
-                print("Warning, accuracy decreased!")
-
-        if maxAcc > bestAcc:
-            bestAcc = maxAcc
-            bestSet = set(bestFeature)
+    for i in range(featureNum - 1, 0, -1):
+        currAcc = 0.0
+        featureToRemove = -1
+        for j in range(1, featureNum + 1):
+            if j in currSet:
+                node = currSet.copy()
+                node.remove(j)
+                temp = accuracy()
+                print(f"Using feature(s) {sorted(node)} accuracy is {temp}%")
+                if temp > currAcc:
+                    currAcc = temp
+                    featureToRemove = j
+        if currAcc > bestAcc:
+            currSet.remove(featureToRemove)
+            bestSet = currSet.copy()
+            bestAcc = currAcc
+            print(f"Feature set {sorted(currSet)} was best, accuracy is {bestAcc}%")
+        elif currAcc < bestAcc:
+            currSet.remove(featureToRemove)
+            print(f"Feature set {sorted(currSet)} was best, accuracy is {currAcc}%")
         else:
+            print(f"Warning: Accuracy cannot be improved further by removing any feature!")
             break
 
-    print(f"\nBest feature subset: {bestSet}")
-    print(f"Best accuracy: {bestAcc}%")
+    if not bestSet:
+        print("Warning: Empty feature set!")
+    print(f"Finished Search! The best feature subset is {bestSet} which has an accuracy of {bestAcc}%")
