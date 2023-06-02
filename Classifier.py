faa70f36e165d3811cec0a3151f353ca7e4fff14
Rayyan Zaid <rayyanzaid0401@gmail.com>
2023-06-02 00:18:29 -0700

    Finished Part 2!!

--- Classifier.py
+++ Classifier.py
@@ -18,19 +18,20 @@ class Classifier:
         self.datasetWithOnlySubsetOfFeatures = datasetWithOnlySubsetOfFeatures
         self.idToRowData = dict()
 
+    def clearDictionary(self):
+        self.idToRowData.clear()
 
     def train(self, trainingInstanceIds: List[int]) -> None:
         
         for rowNumber in trainingInstanceIds:
-            rowIndex = rowNumber - 1
+            rowIndex = rowNumber 
             rowData: List[float] = self.datasetWithOnlySubsetOfFeatures[rowIndex]
             self.idToRowData[rowNumber] = rowData
 
 
     def test(self, test_instance : int) -> float:
 
-        number_correctly_classfied = 0
-        test_index = test_instance - 1
+        test_index = test_instance
 
         label_of_object_to_classify = self.datasetWithOnlySubsetOfFeatures[test_instance-1][0]
         object_to_classify = self.datasetWithOnlySubsetOfFeatures[test_index][1:]
@@ -69,8 +70,4 @@ class Classifier:
         
 
 
-inputData = [[2.0000, 1000,1000 ] , [2.0000, 100,100 ], [1.0000, 1,1 ] , [1.0000, 6.5,7.5 ]  ]
 
-c = Classifier(inputData)
-c.train([1,2,4])
-print(c.test(3))
--- Validator.py
+++ Validator.py
@@ -9,6 +9,7 @@ class Validator:
 	#  classifier : Classifier,
 
 	def __init__(self, featureSubset : List[int] , textfileName : str):
+
 		self.featureSubset : set = featureSubset
 		
 		self.fullDataset : List[List[float]] = read_dataset(textfileName)
@@ -18,13 +19,41 @@ class Validator:
 		self.classifier = Classifier(datasetWithSubsetOfFeatures)
 		
 
-		
+	def calculate_accuracy(self) -> float:
 
-	def calculate_accuracy(self):
 		numberOfRows = len(self.fullDataset)
-		print(numberOfRows)
+		
+		numberOfCorrectPredictions = 0
 
+		listOfTrainingIds = list(range(numberOfRows))
+		# iterate through each ID and exclude it from training
+		for i in range(0,numberOfRows):
+			
+			correctLabel = self.fullDataset[i][0]
 
+			# remove the test ID from list
+			listOfTrainingIds.pop(i)
+			
+			# train model without the test ID
+			self.classifier.train(listOfTrainingIds)
 
-v = Validator([10] , "large-test-dataset-1.txt")
-v.calculate_accuracy()
+			# test the removed ID
+			predictedLabel = self.classifier.test(i)
+
+			if(predictedLabel == correctLabel):
+				numberOfCorrectPredictions += 1
+
+			# put back the removed ID
+			listOfTrainingIds.insert(i,i)
+			
+			# clear dictionary for next use
+			self.classifier.clearDictionary()
+
+		return numberOfCorrectPredictions / numberOfRows
+	
+
+v = Validator([3,5,7] , "small-test-dataset.txt")
+print(v.calculate_accuracy())
+
+v = Validator([1,15,27] , "large-test-dataset-1.txt")
+print(v.calculate_accuracy())
